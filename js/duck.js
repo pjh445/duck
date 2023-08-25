2309d776c2ed1e7de64bdb9621b6f7464a3a6c95
jeonghwa,park <pjh445@gmail.com>
2023-08-25 11:48:30 +0900

    Add files via upload

---
+++ js/duck.js
@@ -0,0 +1,22 @@
+$(document).ready(function(){
+	//첫화면 fade슬라이드
+	const slide = setInterval( fade , 2000 );
+	let i = 3;
+	function fade(){
+		i--; // [2,1,0]
+		if( i == 0){
+			$("#index main img").fadeIn( 1000 );
+			i = 3;
+		} else {
+			$("#index main img").eq( i ).fadeOut( 1000 );
+		}
+	}
+	
+	//상단높이 감지하기
+	const header = $("#header").contentWindow.document;
+	const h = $("#logo").height();
+	alert(h); //151.484 / 123.625
+	
+	
+	
+});/////////////////전체끝
